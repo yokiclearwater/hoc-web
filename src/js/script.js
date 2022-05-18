7a2a39bfa60b1cf67c272a6d620b62fd85fa09c7
YokiClearwater <yonkimkosal@gmail.com>
2022-05-18 16:41:40 +0700

    Added Card Carousel

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,39 @@ $(document).ready(function() {
         // $('.nav-list').toggle();
         $('.nav-list').fadeToggle(300);
     })
+
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      center: true,
+      responsive:{
+          0:{
+              items:1
+          },
+          760:{
+              items:2
+          },
+          1200:{
+              items:3
+          },
+          1600: {
+            items: 4
+          },
+      }
+    })
 });
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString('Welcome To House Of Culture')
+  .pauseFor(300)
+  .start();
+
+
